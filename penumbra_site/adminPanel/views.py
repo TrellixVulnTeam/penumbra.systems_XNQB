c6c80c039357c554e830d1bca1b440dfdaacd819
Clayton <the82professional@hotmail.com>
2018-06-09 15:47:50 -0700

    unified stylesheet update
    Presenting an update to the unified stylesheet.  This version is inherently buggy and should probably not be merged with main but use the older version for stability

--- penumbra_site/adminPanel/views.py
+++
@@ -1,9 +0,0 @@
-from django.http.response import HttpResponse
-from django.template import loader
-
-import adminPanel
-
-def adminPanel(request):
-	template = loader.get_template('adminPanel/adminPanel.html')
-	output = template.render()
-	return HttpResponse(output)
